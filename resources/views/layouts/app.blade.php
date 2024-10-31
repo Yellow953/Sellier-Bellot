e5752e9ff775638d969927925ed7ba3695b77dde
Melany Jabanian <jabanianmelany@gmail.com>
2024-11-01 00:42:15 +0200

    color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,11 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/app-lite.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/core/menu/menu-types/vertical-menu.css') }}">
+    <style>
+        .nav-link:hover, .nav-link:active{
+            color: grey!important;
+        }
+    </style>
 
 </head>
 
